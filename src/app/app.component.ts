dad153269da93f10d7b4b701a78abf036c26cc7a
alemant <tobiasalvarez@yahoo.com.ar>
2022-11-24 17:50:31 -0300

    Changes in components app and components card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit{
+  title = 'Mi primer App Angular';
+  public ArregloTarjetas: Tarjeta[]=[];
+
+  ngOnInit():void {  // Este método se ejecuta cuando se inicializa la página html
+    this.ArregloTarjetas = [
+      {titulo:'Video 1', subtitulo: 'Subtitulo Video 1'},
+      {titulo:'Video 2', subtitulo: 'Subtitulo Video 2'},
+      {titulo:'Video 3', subtitulo: 'Subtitulo Video 3'},
+    ]
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
-
+export class CardComponent implements OnInit {
+  public image:string="https://photoshop-kopona.com/uploads/posts/2019-05/1559108923_0-2.jpg";
+  public Titulo:string ="Curso de Angular con interpolación";
+  constructor() { }
+  ngOnInit(): void {
+  }
 }
